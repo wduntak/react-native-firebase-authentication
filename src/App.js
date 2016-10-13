6887be7c6150a377b13df5a20125ccbf1f9ee5c7
wduntak <wduntak@gmail.com>
2016-10-12 20:32:50 -0400

    Add firebase and firebase config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+	componentWillMount(){
+		firebase.initializeApp({
+	    apiKey: 'AIzaSyCaHVtjrdsew_M1OLtsP7VVbznJnq7PicI',
+	    authDomain: 'react-authentication.firebaseapp.com',
+	    databaseURL: 'https://react-authentication.firebaseio.com',
+	    storageBucket: 'react-authentication.appspot.com',
+	    messagingSenderId: '275980880544'
+		});
+	}
+
 	render() {
 		return (
 			<View>

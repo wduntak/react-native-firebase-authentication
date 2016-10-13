92d362afb6a2ada127442ce4f13ec93d24ee7984
wduntak <wduntak@gmail.com>
2016-10-12 22:28:53 -0400

    Add styles for input fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { text: '' };
+	state = { email: '' };
 
 	render() {
 		return(
 			<Card>
 				<CardSection>
 					<Input 
-						label={'Email'}
-						value={this.state.text}
-						onChangeText = {text => this.setState({ text })}
+						label="Email"
+						placeholder="username@gmail.com"
+						value={this.state.email}
+						onChangeText = {email => this.setState({ email })}
 					/>
 				</CardSection>
 				<CardSection>

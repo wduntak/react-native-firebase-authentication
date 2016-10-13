3978cf6e460f7eaa067dc8c06a3370f17e8b0d67
wduntak <wduntak@gmail.com>
2016-10-13 16:21:39 -0400

    Handle authentication state with switch statement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null };
 	componentWillMount() {
 		firebase.initializeApp({
 	    apiKey: 'AIzaSyCaHVtjrdsew_M1OLtsP7VVbznJnq7PicI',
@@ -13,13 +14,32 @@ class App extends Component {
 	    storageBucket: 'react-authentication.appspot.com',
 	    messagingSenderId: '275980880544'
 		});
+	
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+	
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return <Button>Log out</Button>;
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size="large" />;
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
